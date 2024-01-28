c1666f27681affb65e3900fbc65873a78cce0048
SilberbauerFlo <florian.silberbauer@htl-wels.at>
2024-01-28 22:18:40 +0100

    changed some stuff

--- Release/README.txt
+++ Release/README.txt
@@ -1,3 +1,3 @@
-put a 24bit Bitmap 6.0 into the folder,
+put a 24bit Bitmap into the folder,
 write the name of that file into the file.txt
 and run the "BMP to C array" application
--- main.cpp
+++ main.cpp
@@ -12,12 +12,10 @@ int main(void) {
 	char BmpName[BMP_NAME_SIZE] = {0};  //used to store the name of the bitmap
 	fpos_t NameLength = 0;              //used to rewrite the bitmaps' name for further use
 
-	uint32_t BmpWidth = 0;
-	uint32_t BmpHeight = 0;
-
+	uint32_t BmpSize = 0;
 	/*
-	 * these variables determine the length of the array used to transfer the data
-	 * from the bitmap to the header file
+	 * this variable determines the length of the array used
+	 * to transfer the data from the bitmap to the header file
 	 */
 
 	file = fopen("file.txt","r");       //opens the file.txt in reading mode
@@ -26,13 +24,15 @@ int main(void) {
 	fclose(file);
 
 	bmp = fopen(BmpName,"r");  //opens the bitmap using the name obtained from the file.txt
-	fseek(bmp,0x12,SEEK_SET);  //moves the cursor to the address 0x12 (the first byte of the width of a bitmap)
-	fread(&BmpWidth,4,1,bmp);  //reads 4 bytes (the size of the width of a bitmap)
-	fread(&BmpHeight,4,1,bmp); //reads the next 4 bytes (the size of the height of a bitmap)
+	fseek(bmp,0x2,SEEK_SET);   //moves the cursor to the address 0x2 (the first byte of the size of a bitmap)
+	fread(&BmpSize,4,1,bmp);   //reads 4 bytes (the size of a bitmap)
 
-	uint8_t BMP[BmpHeight*BmpWidth*4];      //creates an array to store all the data of the bitmap
-	fseek(bmp,0,SEEK_SET);                  //moves the cursor back to the start within the bitmap
-	fread(BMP,1,BmpHeight*BmpWidth*4,bmp);  //reads the whole bitmap and puts it into the array
+		printf("Filename = %s\n", BmpName); //outputs some data
+		printf("FileSize = %u", BmpSize);
+
+	uint8_t BMP[BmpSize];       //creates an array to store all the data of the bitmap
+	fseek(bmp,0,SEEK_SET);      //moves the cursor back to the start within the bitmap
+	fread(BMP,1, BmpSize,bmp);  //reads the whole bitmap and puts it into the array
 	fclose(bmp);
 
 	BmpName[NameLength-3] = 'h';
@@ -47,12 +47,12 @@ int main(void) {
 
 	cArr = fopen(BmpName,"w"); //creates a new file using the modified name in writing mode
 
-	BmpName[NameLength-4] = 0;                  //rewrites the dot with NULL
+	BmpName[NameLength-4] = 0;                  //rewrites the dot with a NULL character
 	fprintf(cArr,"const unsigned char ");       //prints the first few words into the header file
 	fprintf(cArr,BmpName);                      //prints the modified name and stops when it encounters a NULL character
 	fprintf(cArr,"[] = {\n  0x%.2x",BMP[0]);    //prints the first byte
 
-	for(uint32_t i = 1; i < BmpHeight*BmpWidth*4; i++) { //prints the rest of the data and starts a new line after every 12 bytes
+	for(uint32_t i = 1; i < BmpSize; i++) { //prints the rest of the data and starts a new line after every 12 bytes
 		fprintf(cArr,", ");
 		if(!(i % 12)) fprintf(cArr,"\n  ");
 		fprintf(cArr,"0x%.2x",BMP[i]);
