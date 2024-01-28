3f195c6f59b028507d41fcfcd60a5b9950adfac1
SilberbauerFlo <florian.silberbauer@htl-wels.at>
2024-01-28 19:32:28 +0100

    made the comments more readable

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ int main(void) {
 	FILE *bmp;  //the bitmap file
 	FILE *cArr; //the output (a header file)
 
-	char BmpName[BMP_NAME_SIZE] = {0}; 	//used to store the name of the bitmap
-	fpos_t NameLength = 0;				//used to rewrite the bitmaps' name for further use
+	char BmpName[BMP_NAME_SIZE] = {0};  //used to store the name of the bitmap
+	fpos_t NameLength = 0;              //used to rewrite the bitmaps' name for further use
 
 	uint32_t BmpWidth = 0;
 	uint32_t BmpHeight = 0;
@@ -19,19 +19,19 @@ int main(void) {
 	 * from the bitmap to the header file
 	 */
 
-	file = fopen("file.txt","r"); 		//opens the file.txt in reading mode
-	fgets(BmpName,BMP_NAME_SIZE,file); 	//reads the bitmaps' name
-	fgetpos(file,&NameLength);			//reads the length of the bitmaps' name by checking how far the cursor has moved
+	file = fopen("file.txt","r");       //opens the file.txt in reading mode
+	fgets(BmpName,BMP_NAME_SIZE,file);  //reads the bitmaps' name
+	fgetpos(file,&NameLength);          //reads the length of the bitmaps' name by checking how far the cursor has moved
 	fclose(file);
 
-	bmp = fopen(BmpName,"r"); 	//opens the bitmap using the name obtained from the file.txt
-	fseek(bmp,0x12,SEEK_SET); 	//moves the cursor to the address 0x12 (the first byte of the width of a bitmap)
-	fread(&BmpWidth,4,1,bmp); 	//reads 4 bytes (the size of the width of a bitmap)
-	fread(&BmpHeight,4,1,bmp);	//reads the next 4 bytes (the size of the height of a bitmap)
+	bmp = fopen(BmpName,"r");  //opens the bitmap using the name obtained from the file.txt
+	fseek(bmp,0x12,SEEK_SET);  //moves the cursor to the address 0x12 (the first byte of the width of a bitmap)
+	fread(&BmpWidth,4,1,bmp);  //reads 4 bytes (the size of the width of a bitmap)
+	fread(&BmpHeight,4,1,bmp); //reads the next 4 bytes (the size of the height of a bitmap)
 
-	uint8_t BMP[BmpHeight*BmpWidth*4];		//creates an array to store all the data of the bitmap
-	fseek(bmp,0,SEEK_SET); 					//moves the cursor back to the start within the bitmap
-	fread(BMP,1,BmpHeight*BmpWidth*4,bmp); 	//reads the whole bitmap and puts it into the array
+	uint8_t BMP[BmpHeight*BmpWidth*4];      //creates an array to store all the data of the bitmap
+	fseek(bmp,0,SEEK_SET);                  //moves the cursor back to the start within the bitmap
+	fread(BMP,1,BmpHeight*BmpWidth*4,bmp);  //reads the whole bitmap and puts it into the array
 	fclose(bmp);
 
 	BmpName[NameLength-3] = 'h';
@@ -44,10 +44,10 @@ int main(void) {
 	 */
 	cArr = fopen(BmpName,"w"); //creates a new file using the modified name in writing mode
 
-	BmpName[NameLength-4] = 0; 					//rewrites the dot with NULL
-	fprintf(cArr,"const unsigned char "); 		//prints the first few words into the header file
-	fprintf(cArr,BmpName); 						//prints the modified name and stops when it encounters a NULL character
-	fprintf(cArr,"[] = {\n  0x%.2x",BMP[0]); 	//prints the first byte
+	BmpName[NameLength-4] = 0;                  //rewrites the dot with NULL
+	fprintf(cArr,"const unsigned char ");       //prints the first few words into the header file
+	fprintf(cArr,BmpName);                      //prints the modified name and stops when it encounters a NULL character
+	fprintf(cArr,"[] = {\n  0x%.2x",BMP[0]);    //prints the first byte
 
 	for(uint32_t i = 1; i < BmpHeight*BmpWidth*4; i++) { //prints the rest of the data and starts a new line after every 12 bytes
 		fprintf(cArr,", ");
